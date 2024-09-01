4a766991704b8d7f9ce6c58c35bf8e4391df7438
NilsAstrup <168202734+NilsAstrup@users.noreply.github.com>
2024-09-01 22:42:22 +0200

    Create 2.11b.py
    Code

---
+++ 1/2.11b.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+N=21
+x=np.linspace(-3,3,N)
+y=np.linspace(-2,3,N)
+
+rx,ry=np.meshgrid(x,y,indexing="ij")
+
+a=1
+Ex=rx.copy()
+Ey=ry.copy()
+
+for i in range(len(rx.flat)):
+    Ex.flat[i],Ey.flat[i]=0,1/(ry.flat[i]*(ry.flat[i]-a))
+
+Emag=np.sqrt(Ex**2+Ey**2)
+
+nEx=Ex/Emag
+nEy=Ey/Emag
+
+plt.quiver(rx, ry, nEx, nEy, np.log10(Emag),cmap="jet")
+plt.colorbar()
+plt.show()
